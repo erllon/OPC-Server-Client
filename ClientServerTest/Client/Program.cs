af0c8a832811db95f0719ef272c7d228f154dd3f
Erlend Lone <erlend.lone@no.abb.com>
2019-06-20 16:02:09 +0200

    Adding MyNodeManager

--- ClientServerTest/Client/Program.cs
+++ ClientServerTest/Client/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Collections.Generic;
- 
+using System.Linq;
 using Opc.UaFx;
 using Opc.UaFx.Client;
 using Opc.UaFx.Services;
@@ -16,11 +16,58 @@ public class Program {
     {
       client.Connect();
 
-      while (true)//(!exitSession)
+      while (!exitSession)
       {
+        List<OpcNodeId> ids = new List<OpcNodeId>();
+        List<OpcValue> values = new List<OpcValue>();
+        List<OpcBrowseNode> browseList = new List<OpcBrowseNode>();
+        List<OpcNodeId> listOfIds = new List<OpcNodeId>();
+        //List<OpcNodeInfo> infoList = client.BrowseNode("ns=2;Main");
+        browseNodes(client, listOfIds);
+        readNodes(client,listOfIds);
+        
+        //List<OpcNodeInfo> infoList = client.BrowseNodes(browseList);
+        //Dictionary<OpcNodeId,Opcvariable>
+        // client.BrowseNodes
+        // OpcValue lestVerdi;
+        //ids.Add("ns=2;s=Main/Level");
+        //Console.WriteLine($"--------------------{ids[0].ValueAsString}-----------------");
+        values = client.ReadNodes(listOfIds).ToList();
+        
+        //lestVerdi = client.ReadNode("2:");
+        Console.WriteLine("\n\nWriting the content of values-list...");
+        foreach(OpcValue value in values)
+        {
+          Console.WriteLine($"ID: {value.DataTypeId} Data type: {value.DataType} Value: {value.Value}");
+        }
+        Console.WriteLine("Finished writing the content of values-list...\n\n");
+
+        OpcNodeInfo mainNode = client.BrowseNode("2:Main");
+
+        OpcValue valueRead = client.ReadNode("ns=2;s=Main/Level");
+        Console.WriteLine($"ValueRead1: {valueRead.Value}");
+        
+        client.WriteNode("ns=2;s=Main/Level",2000);
+        valueRead = client.ReadNode("ns=2;s=Main/Level");
+
+        Console.WriteLine($"ValueRead2: {valueRead.Value}");
+
+        OpcValue readVal = client.ReadNode("Main/Level");
+
+        Console.WriteLine($"ValueRead: {valueRead.Value}");
+        Console.WriteLine($"ReadVal: {readVal.Value}");
+
+        foreach (var childNode in mainNode.Children())
+        {
+          valueRead = client.ReadNode(childNode.NodeId);
+          Console.WriteLine($"Name: {childNode.Name}\t\t\tValue: {valueRead}");
+        }
+
+
         Console.WriteLine("Available commands: 'view' 'edit' 'disconnect'");
         Console.Write("Enter a command from the above list: ");
         stringEntered = Convert.ToString(Console.ReadLine());
+        
 
         switch(stringEntered)
         {
@@ -28,11 +75,11 @@ public class Program {
           {
             Console.WriteLine("Enter the name(s) of the node(s) separated by whitespace(s):");
             string fullString = Console.ReadLine();
-            List<OpcNodeInfo> nodeInfoList = AddNodesToList(fullString, client);
+            List<OpcNodeInfo> nodeInfoList = FindValidNodes(fullString, client);
             Console.WriteLine("****************************");
             foreach(OpcNodeInfo infoElement in nodeInfoList)
             {
-              //Start here!
+              Console.WriteLine($"ID: {infoElement.NodeId}\tName: {infoElement.Name}\tDisplay name: {infoElement.DisplayName}\tCategory: {infoElement.Category}\tContext: {infoElement.Context}\tReference: {infoElement.Reference}");
             }
             Console.WriteLine("****************************");
 
@@ -41,7 +88,10 @@ public class Program {
           }
           case "editValue":
           {
-           //EditNodeValue();
+            Console.WriteLine("Enter the name(s) of the node(s) separated by whitespace(s):");
+            string editString = Console.ReadLine();
+            // List<OpcNode> nodeInfoList = FindValidNodes(fullString, client);
+            // EditNodeValue();
             break;
           }          
           case "disconnect":
@@ -54,47 +104,9 @@ public class Program {
           default:
           {
             Console.WriteLine(stringEntered + " is not an accepted command");
+            client.WriteNode("ns=2;s=Main/Level",12);
             break;
           }            
-        }
-        string[] splittedString = stringEntered.Split(" ");
-        listOfNodes = new List<OpcNodeInfo>(); //or initialize it when declared and clear the list here?
-        OpcNodeInfo machineNode;
-        foreach(string substring in splittedString)
-        {
-          machineNode = client.BrowseNode($"ns=2;");
-          // if(!machineNode.Name.IsNull)
-          // {
-            OpcNodeInfo jobnode = machineNode.Child("Job");
-            listOfNodes.Add(machineNode);
-          //}
-        }
-        OpcBrowseNode test = new OpcBrowseNode("s=Message");
-        IEnumerable<OpcNodeInfo> infoAboutNodes = client.BrowseNodes(test);
-        foreach(string element in splittedString)
-        {
-          List<OpcReadNode> liste = new List<OpcReadNode>();
-          liste.Add(new OpcReadNode("s=Message"));
-          //OpcNodeInfo info = client.BrowseNode($"ns=2;s=Message");
-          List<OpcBrowseNode> liste2 = new List<OpcBrowseNode>();
-          //OpcBrowseNode test = new OpcBrowseNode("s=Message");
-          IEnumerable<OpcNodeInfo> info = client.BrowseNodes(test);
-          Console.WriteLine("\n**********************************");
-          Console.WriteLine("Writing the infoElements...");
-          Console.WriteLine("**********************************\n");
-
-          foreach(OpcNodeInfo infoElement in info)
-          {
-            Console.WriteLine("NodeID: " + infoElement.NodeId);
-            Console.WriteLine("InfoElement:: " + infoElement.ToString() + "\n");
-
-          }
-          var nodeOfInterest = client.ReadNode($"ns=2;s={stringEntered}");
-          if(nodeOfInterest.Value != null)
-          {
-            Console.Write($"The value of the node is: {nodeOfInterest.Value}\t");
-            Console.WriteLine($"The ID of the node is: {nodeOfInterest.DataTypeId}\n");
-          }
         }
         
         if(client.State == OpcClientState.Connected)
@@ -112,12 +124,61 @@ public class Program {
     }
   }
 
-    private static void DisplayNodeInfo(List<OpcNodeInfo> nodeInfoList)
+    //TODO: Lag dictionary med id som nøkkel og opcdatavariablenode som value
+    //      Da skal det være lettere å gå gjennom alle nodene i idList
+    private static void readNodes(OpcClient opcClient, List<OpcNodeId> idList)
     {
-        throw new NotImplementedException();
+      List<OpcValue> valueList = opcClient.ReadNodes(idList).ToList();
+      Console.WriteLine("**********************");
+      Console.WriteLine($"READING NODES");
+      Console.WriteLine("**********************");
+      Console.WriteLine("\nWriting values of all childnodes of " + valueList[0]);
+      // foreach(var subNode in mainInfo.)
+      // {
+      //   Console.WriteLine($"NodeID: {subNode.NodeId}\t\tName: {subNode.Name}\t\tCategory: {subNode.Category}");
+      // }
+      // Console.WriteLine("Finished writing all childnodes of " + mainInfo.DisplayName);
     }
 
-    private static List<OpcNodeInfo> AddNodesToList(string nodesEntered, OpcClient opcClient)
+    private static void browseNodes(OpcClient opcClient, List<OpcNodeId> idList)
+    {      
+      OpcNodeInfo mainInfo = opcClient.BrowseNode("ns=2;Main");
+      Console.WriteLine("**********************");
+      Console.WriteLine($"BROWSING NODE: {mainInfo.DisplayName}");
+      Console.WriteLine("**********************");
+
+      
+      Console.WriteLine("\n\nWriting all childnodes of " + mainInfo.DisplayName + ":");
+      foreach(var subNode in mainInfo.Children())
+      {
+        Console.WriteLine($"NodeID: {subNode.NodeId}\t\tName: {subNode.Name}\t\tCategory: {subNode.Category}");
+        idList.Add(subNode.NodeId);
+      }
+      Console.WriteLine("Finished writing all childnodes of " + mainInfo.DisplayName);
+    }
+
+    private static void EditNodeValue(string enteredString, OpcClient client)
+    {
+        string[] nodeArray = enteredString.Split(' ');
+        List<OpcNodeInfo> returnList = new List<OpcNodeInfo>();
+        OpcNodeInfo nodeInfo;
+        foreach(string substring in nodeArray)
+        {
+          nodeInfo = client.BrowseNode($"ns=2;s={substring}");
+          if(!nodeInfo.Name.IsNull)
+          {
+            returnList.Add(nodeInfo);
+          }
+        }
+        //return returnList;|
+    }
+
+    private static void DisplayNodeInfo(List<OpcNodeInfo> nodeInfoList)
+    {
+
+    }
+
+    private static List<OpcNodeInfo> FindValidNodes(string nodesEntered, OpcClient opcClient)
     {
         string[] nodeArray = nodesEntered.Split(' ');
         List<OpcNodeInfo> returnList = new List<OpcNodeInfo>();
--- ClientServerTest/Server/Program.cs
+++ ClientServerTest/Server/Program.cs
@@ -3,26 +3,42 @@ using System;
 using Opc.UaFx;
 using Opc.UaFx.Server;
 using System.Collections.Generic;
-
+using System.Linq;
 
 class Program
 {
+  static OpcFolderNode mainNode;
+  static OpcDataVariableNode<double> mainTemperatureNode;
   static int connectedClients;
   public static void Main()
   {
+    
     connectedClients = 0;
     List<OpcDataVariableNode> nodeList = new List<OpcDataVariableNode>();
     var temperatureNode = new OpcDataVariableNode<double>("Temperature", 100.0);
     var messageNode = new OpcDataVariableNode<string>("Message", string.Empty);
     var levelDetectionNode = new OpcDataVariableNode<bool>("Level", false);
+
+    mainNode = new OpcFolderNode("Main");
+    var mainIsRunningNode = new OpcDataVariableNode<bool>(mainNode, "IsRunning");
+    var mainMessageNode = new OpcDataVariableNode<string>(mainNode,"Message");
+    mainTemperatureNode = new OpcDataVariableNode<double>(mainNode,"Temperature");
+    var mainAlarmNode = new OpcDataVariableNode<bool>(mainNode,"Alarm");
+    var mainLevelNode = new OpcDataVariableNode<double>(mainNode,"Level");
     
+    
+
     bool avslutt= false;
 
     nodeList.Add(levelDetectionNode);
     nodeList.Add(temperatureNode);
     nodeList.Add(messageNode); 
-    
-    using (var server = new OpcServer("opc.tcp://localhost:4840/", nodeList)) 
+    MyNodeManager nodeManager = new MyNodeManager();
+    nodeManager.MonitoredItemsModified += new OpcMonitoredItemsEventHandler((sender,e) => blablametode(sender,e));
+    //nodeManager.CreateNodes(new OpcNodeReferenceCollection());
+    //using (var server = new OpcServer("opc.tcp://localhost:4840/", nodeList)) 
+    //using (var server = new OpcServer("opc.tcp://localhost:4840/", mainNode))
+    using(var server = new OpcServer("opc.tcp://localhost:4840/", nodeManager))
     {   
       server.Started += new EventHandler((sender, e) => ServerStarted(sender, e, nodeList, server));
       // server.RequestProcessing += new OpcRequestProcessingEventHandler((a, b) => reqProcessing(a,b));
@@ -31,10 +47,18 @@ class Program
       server.SessionCreated += new OpcSessionEventHandler((serr,rerrr)=>sessionCreatedMethod(serr,rerrr));
       server.SessionClosing += new OpcSessionEventHandler((sender, e) => SessionClosingMethod(sender,e));
 
+      
       server.Start();
-
+      foreach(var node in nodeManager.Nodes)
+      {
+      }
+      Console.WriteLine("The server is running...");
+      using(new Timer(UpdateTemperture,server,TimeSpan.Zero,TimeSpan.FromSeconds(1)))
+      //Console.ReadKey();
+      //var testVar = nodeManager.Nodes.Contains("ns=2;s=Level");
       while (true)
-      {        
+      {
+                
         if (temperatureNode.Value == 110)
             temperatureNode.Value = 100;
         else
@@ -49,11 +73,31 @@ class Program
         else
           levelDetectionNode.Value = true;
 
-
         Thread.Sleep(1000);
       }
     }
   }
+
+    private static void sdlkad(object s, OpcNodeAccessEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void blablametode(object sender, OpcMonitoredItemsEventArgs e)
+    {
+        Console.WriteLine("En node-verdi i samlingen har blitt endret!");
+    }
+
+    private static void UpdateTemperture(object state)
+    {
+      var server = (OpcServer)state;
+      var mnode = mainNode;
+
+      mainTemperatureNode.Value = DateTime.Now.Second/3.0;
+      mainTemperatureNode.ApplyChanges(server.SystemContext);
+      Console.WriteLine("Temperature is: " + mainTemperatureNode.Value.ToString("N2"));
+      Thread.Sleep(1000);
+    }
 
     private static void sessionCreatedMethod(object sender, OpcSessionEventArgs eArgs)
     {
@@ -120,4 +164,27 @@ class Program
 
       listOfNodes[0].ApplyChanges(opcServer.SystemContext);
     }
+}
+public class MyNodeManager : OpcNodeManager
+{
+  public IEnumerable<IOpcNode> noder{get;set;}
+ public MyNodeManager() : base("http://mynamespace/")
+ {
+    noder = this.CreateNodes(new OpcNodeReferenceCollection());
+ }
+ protected override IEnumerable<IOpcNode> CreateNodes(OpcNodeReferenceCollection references)
+  {
+    // Define custom root node.
+      var mainNode = new OpcFolderNode(new OpcName("Main", this.DefaultNamespaceIndex));
+      // Add custom root node to the Objects-Folder (the root of all server nodes):
+      references.Add(mainNode, OpcObjectTypes.ObjectsFolder);
+      // Add custom sub node beneath of the custom root node:
+      var isMachineRunningNode = new OpcDataVariableNode<bool>(mainNode,"IsRunning");
+      var mainMessageNode = new OpcDataVariableNode<string>(mainNode,"Message","Meldingsverdi");
+      var mainAlarmNode = new OpcDataVariableNode<bool>(mainNode,"Alarm");
+      var mainLevelNode = new OpcDataVariableNode<double>(mainNode,"Level");
+      // Return each custom root node using yield return.
+      yield return mainNode;
+      //Mer kode her
+  }
 }
